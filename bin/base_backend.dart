3372e94ef29bc40d26f375ff18babfc9f1a75f8b
Gustav0G <newguguwow@gmail.com>
2022-07-17 04:58:02 -0300

    Add Dependencies

--- bin/base_backend.dart
+++ bin/base_backend.dart
@@ -1,3 +1,11 @@
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
+import 'package:mysql1/mysql1.dart';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  serve(
+      (Request req) => Response(200,
+          body: 'Olá mundo', headers: {'content-type': 'application/json'}),
+      'localhost',
+      8080);
 }
